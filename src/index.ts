f23bb09427ac7044eaa50c6c34aab081776de57f
Lyrkan <850046+Lyrkan@users.noreply.github.com>
2024-11-02 00:33:03 +0100

    Remove unused package

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,6 @@ import { app, BrowserWindow, session } from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) {
-  app.quit();
-}
-
 const isDev = () => process.env['WEBPACK_SERVE'] === 'true';
 
 const createWindow = (): void => {
