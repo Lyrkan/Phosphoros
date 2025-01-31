b988a270c7ea6e6b70b82feddaf8d9f40e5c29fb
Lyrkan <850046+Lyrkan@users.noreply.github.com>
2025-01-31 22:34:03 +0100

    Try to fix Grbl commands tracking

--- src/providers/SerialServiceProvider.tsx
+++ src/providers/SerialServiceProvider.tsx
@@ -1,25 +1,46 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
 import { useStore } from '../stores/RootStore';
 import { MessageHandlerService } from '../services/MessageHandlerService';
 import { SerialService } from '../services/SerialService';
 import { SerialServiceContextProvider } from '../contexts/SerialServiceContext';
 import { useCommandTracking } from '../hooks/useCommandTracking';
 
-export function SerialServiceProvider({ children }: { children: ReactNode }) {
-  const rootStore = useStore();
-
-  const messageHandler = new MessageHandlerService(rootStore);
-  const serialService = new SerialService(rootStore.serialStore, messageHandler);
-
+function SerialServiceComponent({
+  messageHandler,
+  serialService,
+  children
+}: {
+  messageHandler: MessageHandlerService;
+  serialService: SerialService;
+  children: ReactNode;
+}) {
   const commandTracking = useCommandTracking();
-  messageHandler.setCommandTracking(commandTracking);
 
-  // Set the serial service in the settings store
-  rootStore.settingsStore.setSerialService(serialService);
+  useEffect(() => {
+    messageHandler.setCommandTracking(commandTracking);
+  }, [messageHandler, commandTracking]);
 
   return (
     <SerialServiceContextProvider value={serialService}>
       {children}
     </SerialServiceContextProvider>
+  );
+}
+
+export function SerialServiceProvider({ children }: { children: ReactNode }) {
+  const rootStore = useStore();
+  const messageHandler = new MessageHandlerService(rootStore);
+  const serialService = new SerialService(rootStore.serialStore, messageHandler);
+
+  // Set the serial service in the settings store
+  rootStore.settingsStore.setSerialService(serialService);
+
+  return (
+    <SerialServiceComponent
+      messageHandler={messageHandler}
+      serialService={serialService}
+    >
+      {children}
+    </SerialServiceComponent>
   );
 }
