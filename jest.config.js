0e7be6f90b1b355097c7a6af36d1bf4f8ef5e8f3
Lyrkan <850046+Lyrkan@users.noreply.github.com>
2025-02-08 17:38:05 +0100

    Add coverage report

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,16 @@ module.exports = {
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/__tests__/**',
+    '!src/setupTests.ts',
+    '!src/index.tsx',
+  ],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': '<rootDir>/src/__mocks__/styleMock.js',
     '\\.(gif|ttf|eot|svg)$': '<rootDir>/src/__mocks__/fileMock.js'
